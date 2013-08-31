a3d45167a4dd9c842cc1a8b7fca349d418f321c9
James Moore <moore.work@live.com>
2013-08-31 19:22:09 -0400

    Slight mandelbrot

--- SteeringBehaviors/src/mandelbrot/MandelBrot.java
+++ SteeringBehaviors/src/mandelbrot/MandelBrot.java
@@ -27,7 +27,7 @@ import steeringbehaviors.WorldGraphics2D;
  * @author James Moore (moore.work@live.com)
  */
 public class MandelBrot implements RunnableSim
-  {
+{
 
     private int maxIter = 1000;
     
@@ -42,7 +42,7 @@ public class MandelBrot implements RunnableSim
     BufferedImage buff;
 
     public MandelBrot()
-      {
+    {
         screenSize = Settings.getInstance().getResolution();
         time = 0;
         Random rand = new Random();
@@ -54,33 +54,30 @@ public class MandelBrot implements RunnableSim
         config = device.getDefaultConfiguration();
         buff = config.createCompatibleImage(screenSize.width, screenSize.height, BufferedImage.TYPE_INT_RGB);
 
-      }
+    }
 
-    public static void main(String[] args) throws IOException
-      {
-        MandelBrot man = new MandelBrot();
-        BufferedImage out = man.render();
-        File f = new File("saved2.png");
-        ImageIO.write(out, "png", f);
-      }
+//    public static void main(String[] args) throws IOException
+//    {
+//        MandelBrot man = new MandelBrot();
+//        BufferedImage out = man.render();
+//        File f = new File("saved2.png");
+//        ImageIO.write(out, "png", f);
+//    }
 
     @Override
     public void draw(WorldGraphics2D w2d)
-      {
+    {
         w2d.drawImage(this.render(), Point2D.zero, w2d);
-      }
+    }
 
     @Override
     public void update()
-      {
+    {
         //throw new UnsupportedOperationException("Not supported yet.");
-      }
+    }
 
     public BufferedImage render()
-      {
-
-        //BufferedImage buff2 = new BufferedImage(screenSize.width, screenSize.height, BufferedImage.TYPE_INT_RGB);
-        
+    {
 
         int iterations;
         CyclicBarrier cb = new CyclicBarrier(screenSize.height);
@@ -88,50 +85,49 @@ public class MandelBrot implements RunnableSim
         Thread[] threadz = new pixelIterator[screenSize.height];
 
         for (int y = 0; y < screenSize.height; y++)
-          {
+        {
 
             //TODO get rid of these magic numbers
 
-
-            threadz[y] =  new pixelIterator(y, maxIter, time, buff);
+            threadz[y] = new pixelIterator(y, maxIter, time, buff);
             threadz[y].start();
-            
+
             // iterations = iterateOnAPixel(xScaled, yScaled, maxIter);
 
             //buff.setRGB(x, y, ColorTool.getRGBColor(iterations, iterations / 2, iterations * time));
             //buff.setRGB(x, y, ColorTool.getRGBColor((int) (iterations + redMutator), (int) (iterations / greenMutator), (int) (iterations * blueMutator)));
 
-          }
+        }
         time += 1;
         return buff;
-      }
+    }
 
     private int iterateOnAPixel(double x0, double y0, int max_iteration)
-      {
+    {
         int iteration = 0;
         double x, y;
         x = y = 0;
 
         while ((x * x + y * y) < 4 && iteration < max_iteration)
-          {
+        {
             double xtemp = x * x - y * y + x0;
             y = 2 * x * y + y0;
 
             x = xtemp;
 
             iteration = iteration + 1;
-          }
+        }
 
         return iteration;
-      }
-  }
+    }
+}
 
 //TODO redo thread structore
 //TODO find better way of communicating to the pic 
 class pixelIterator extends Thread
-  {
+{
 
-    private static final int WIDTH = 1920;
+    private static int WIDTH;
     int yIndex;
     double x_0, y_0;
     int max_iter, time;
@@ -145,20 +141,21 @@ class pixelIterator extends Thread
     private CyclicBarrier cb;
 
     public pixelIterator(int yIndex, int max_iter, int time, BufferedImage img)
-      {
+    {
         this.yIndex = yIndex;
         this.max_iter = max_iter;
         this.time = time;
-  
+
         this.img = img;
-      }
+        WIDTH = img.getWidth();
+    }
 
     @Override
-    public void run()
-      {
+    public void start()
+    {
 
         for (int i = 0; i < WIDTH; i++)
-          {
+        {
 
             double xScaled = Scaler.scale((double) screenSize.width, 0.0, MBROT_X_HIGH_BOUND, MBROT_X_LOW_BOUND, i);
             double yScaled = Scaler.scale((double) screenSize.height, 0.0, MROT_Y_HIGH_BOUND, MBROT_Y_LOW_BOUND, yIndex);
@@ -167,20 +164,19 @@ class pixelIterator extends Thread
             double x, y;
             x = y = 0;
             while ((x * x + y * y) < 4 && iteration < max_iter)
-              {
+            {
                 double xtemp = x * x - y * y + xScaled;
                 y = 2 * x * y + yScaled;
 
                 x = xtemp;
 
                 iteration = iteration + 1;
-              }
+            }
 
             img.setRGB(i, yIndex, ColorTool.getRGBColor(iteration, iteration / 2, iteration * time));
-          }
-
-      }
-  }
+        }
+    }
+}
 
 
 //TODO thread creation seems to take a long time? 
