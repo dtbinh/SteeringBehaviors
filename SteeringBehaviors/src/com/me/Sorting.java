e29a3b43d37ce28acf37c36208b55ce8e7ed0eb3
James Moore <moore.work@live.com>
2013-10-13 21:17:37 -0400

    Insertion Sort Complete

--- SteeringBehaviors/src/com/me/Sorting.java
+++ SteeringBehaviors/src/com/me/Sorting.java
@@ -23,186 +23,212 @@ import java.util.logging.Logger;
  * @author Bryan
  */
 public class Sorting implements com.me.steeringbehaviors.RunnableSim
-  {
-
+{
+    
     private static List<Integer> elements;
     private static List<Integer> elements2;
+    private static List<Integer> elements3;
     private static Utility ut;
     private static final int NUM_ELEMENTS = 80;
     private static int index = 0;
-
+    
     public Sorting()
-      {
+    {
         elements = new ArrayList<Integer>();
         elements2 = new ArrayList<Integer>();
-
+        elements3 = new ArrayList<Integer>();
+        
         ut = new Utility();
-
+        
         Random r = new Random();
         for (int i = 0; i < NUM_ELEMENTS; i++)
-          {
-            elements.add(r.nextInt(100));
-            elements2.add(r.nextInt(100));
-          }
-
+        {
+            int val = r.nextInt(150);
+            elements.add(val);
+            elements2.add(val);
+            elements3.add(val);
+        }
+        
         index = elements.size() - 1;
-      }
-
+    }
+    
     @Override
     public void draw(WorldGraphics2D w2d)
-      {
+    {
         Dimension res = Settings.getInstance().getResolution();
         int offset = 10;
         for (int i = 0; i < NUM_ELEMENTS; i++)
-          {
+        {
             w2d.drawRectangle(new Point2D(i * offset, 600), new Dimension(10, 2 * elements.get(i)), Color.blue);
             w2d.drawRectangle(new Point2D(i * offset, 300), new Dimension(10, 2 * elements2.get(i)), Color.red);
-
-          }
-
-      }
-
+            w2d.drawRectangle(new Point2D(i * offset, 50), new Dimension(10, 2 * elements3.get(i)), Color.yellow);
+            
+        }
+        
+    }
+    
     @Override
     public void update()
-      {
+    {
         try
-          {
-
+        {
+            
             this.runOneStepSelectionSort(elements, index);
             this.runOneBubbleSort(elements2);
+            elements3 = this.runOneStepInsertionSort(elements3, index);
             index = index - 1;
             Thread.sleep(100);
-
-            if (this.isUnSorted(elements) && this.isUnSorted(elements2))
-              {
+            
+            if (this.isUnSorted(elements) && this.isUnSorted(elements2) && this.isUnSorted(elements3))
+            {
                 System.exit(0);
-              }
-          }
+            }
+        }
         catch (InterruptedException ex)
-          {
+        {
             Logger.getLogger(Sorting.class.getName()).log(Level.SEVERE, null, ex);
-          }
-      }
-
+        }
+    }
+    
     public List<Integer> selectionSortComplete(List<Integer> sortee)
-      {
+    {
         int index = sortee.size() - 1;
         while (isUnSorted(sortee))
-          {
+        {
             runOneStepSelectionSort(sortee, index);
             index -= 1;
-          }
+        }
         return sortee;
-      }
-
+    }
+    
     public List<Integer> bubbleSortComplete(List<Integer> sortee)
-      {
+    {
         while (isUnSorted(sortee))
-          {
+        {
             runOneBubbleSort(sortee);
-          }
+        }
         return sortee;
-      }
-
+    }
+    
     private boolean isUnSorted(List<Integer> sortee)
-      {
+    {
         if (sortee.size() < 2)
-          {
+        {
             return true;
-          }
-
+        }
+        
         for (int i = 1; i < sortee.size(); i++)
-          {
+        {
             if (sortee.get(i - 1) > sortee.get(i))
-              {
+            {
                 return false;
-              }
-
-          }
+            }
+            
+        }
         return true;
-      }
-
+    }
+    
     protected void runOneBubbleSort(List<Integer> sortee)
-      {
-
-
+    {
+        
+        
         if (sortee.size() < 1)
-          {
+        {
             System.out.println("Sortee is rather small");
-          }
+        }
         for (int i = 1; i < sortee.size(); i++)
-          {
+        {
             if (sortee.get(i) < sortee.get(i - 1))
-              {
-
+            {
+                
                 Collections.swap(sortee, i, i - 1);
-              }
-          }
-
-
-      }
-
+            }
+        }
+        
+        
+    }
+    
     public static List<Integer> getElements()
-      {
+    {
         return elements;
-      }
-
+    }
+    
     protected void runOneStepSelectionSort(List<Integer> sortee, int index)
-      {
+    {
         int currMaxIndex = 0;
-
+        
         if (sortee.size() < 1)
-          {
+        {
             System.out.println("Sortee is rather small");
-          }
-
+        }
+        
         for (int i = 0; i <= index; i++)
-          {
+        {
             if (sortee.get(i) > sortee.get(currMaxIndex))
-              {
+            {
                 currMaxIndex = i;
-              }
-          }
+            }
+        }
         Collections.swap(sortee, currMaxIndex, index);
-      }
+    }
 
-    protected void runOneStepInsertionSort(List<Integer> sortee, int index)
-      {
+    //TODO wrong method name
+    protected List<Integer> runFullInsertionSort(List<Integer> sortee)
+    {
+        int index = sortee.size() - 1;
+        while (isUnSorted(sortee) == false)
+        {
+            sortee = runOneStepInsertionSort(sortee, index);
+            index -= 1;
+        }
+        return sortee;
+    }
+    
+    protected List<Integer> runOneStepInsertionSort(List<Integer> sortee, int index)
+    {
         int currMaxIndex = 0;
-
+        
+        
         if (sortee.size() < 1)
-          {
+        {
             System.out.println("Sortee is rather small");
-          }
+        }
 //TODO do this correctly 
 //6, 5, 3, 1, 8, 7, 2, 4        
         int val = sortee.get(index);
         for (int i = sortee.size() - 1; i > index; i--)
-          {
+        {
             if (val >= sortee.get(i))
-              {
-      
+            {
+                if (val == 3)
+                {
+                    System.out.println("foo");
+                }
+                
                 sortee = insertAt(sortee, i + 1, val);
-                break;
-              }
-          }
-      }
-
+                sortee.remove(index);
+                return sortee;
+            }
+        }
+        
+        return sortee;
+    }
+    
     public static List<Integer> insertAt(List<Integer> listToInsert, int index, Integer val)
-      {
+    {
         List<Integer> ret = new LinkedList<Integer>();
-        for (int i = 0; i < index -1 ; i++)
-          {
+        for (int i = 0; i < index; i++)
+        {
             ret.add(listToInsert.get(i));
-          }
-
+        }
+        
         ret.add(val);
-
-        for (int i = index + 1; i < listToInsert.size(); i++)
-          {
+        
+        for (int i = index; i < listToInsert.size(); i++)
+        {
             ret.add(listToInsert.get(i));
-          }
-
+        }
+        
         return ret;
-      }
-  }
+    }
+}
--- SteeringBehaviors/test/com/me/SortingTest.java
+++ SteeringBehaviors/test/com/me/SortingTest.java
@@ -16,46 +16,36 @@ import static org.junit.Assert.*;
  * @author James Moore (moore.work@live.com)
  */
 public class SortingTest
-  {
+{
 
-    private static List<Integer> elements; private Sorting sort;
+    private static List<Integer> elements;
+    private Sorting sort;
 
     public SortingTest()
-      {
+    {
         sort = new Sorting();
-      }
+    }
 
     @Before
     public void setUp()
-      {
+    {
         //65318724
         Integer[] els =
-          {
+        {
             6, 5, 3, 1, 8, 7, 4
-          };
-        elements = Arrays.asList(els);
-      }
+        };
+        elements =  new ArrayList<Integer>(Arrays.asList(els)) ;
+    }
 
     @Test
     public void testInsertionSort()
-      {
+    {
         Integer[] els =
-          {
+        {
             6, 5, 3, 1, 8, 7, 4
-          };
-       sort.runOneStepInsertionSort(elements, elements.size() - 1);
-
-        for (int i = 0; i < els.length; i++)
-          {
-            assertEquals(els[i], elements.get(i));
-          }
-        
-         sort.runOneStepInsertionSort(elements, elements.size() - 2);
-         
-          System.out.println(elements);
-          
-          sort.runOneStepInsertionSort(elements, elements.size() - 3);
-          System.out.println(elements);
-          fail("");
-      }
-  }
+        };
+ 
+        elements =sort.runFullInsertionSort(elements);
+      
+    }
+}
