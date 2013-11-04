a9523c8260e465ea8d231f730a6380d409e119bc
moorework <moore.work@live.com>
2013-11-03 22:04:24 -0500

    implemented merge sort and some testing.  No visualizations yet

--- SteeringBehaviors/test/com/me/SortingTest.java
+++ SteeringBehaviors/test/com/me/SortingTest.java
@@ -15,106 +15,108 @@ import static org.junit.Assert.*;
  *
  * @author James Moore (moore.work@live.com)
  */
-public class SortingTest
-  {
+public class SortingTest {
 
     private static List<Integer> elements;
     private Sorting sort;
 
-    public SortingTest()
-      {
+    public SortingTest() {
         sort = new Sorting();
-      }
+    }
 
     @Before
-    public void setUp()
-      {
+    public void setUp() {
         //65318724
-        Integer[] els =
-          {
+        Integer[] els = {
             6, 5, 3, 1, 8, 7, 4
-          };
+        };
         elements = new ArrayList<Integer>(Arrays.asList(els));
-      }
+    }
 
     @Test
-    public void testInsertionSort()
-      {
-        Integer[] els =
-          {
+    public void testInsertionSort() {
+        Integer[] els = {
             6, 5, 3, 1, 8, 7, 4
-          };
+        };
 
         elements = sort.runOneStepInsertionSort(elements, els.length - 1);
         assertTrue(twoArraysSame(els, elements));
 
-        Integer[] els2 =
-          {
+        Integer[] els2 = {
             6, 5, 3, 1, 8, 4, 7
-          };
+        };
 
         elements = sort.runOneStepInsertionSort(elements, els.length - 2);
         assertTrue(twoArraysSame(els2, elements));
 
-        Integer[] els3 =
-          {
+        Integer[] els3 = {
             6, 5, 3, 1, 4, 7, 8
-          };
+        };
 
         elements = sort.runOneStepInsertionSort(elements, els.length - 3);
         assertTrue(twoArraysSame(els3, elements));
 
 
-        Integer[] els4 =
-          {
+        Integer[] els4 = {
             6, 5, 3, 1, 4, 7, 8
-          };
+        };
 
         elements = sort.runOneStepInsertionSort(elements, els.length - 4);
         assertTrue(twoArraysSame(els4, elements));
-        System.out.println(elements);
-          
 
-        Integer[] els5 =
-          {
-             6, 5, 1, 3, 4, 7, 8
-          };
+
+
+        Integer[] els5 = {
+            6, 5, 1, 3, 4, 7, 8
+        };
 
         elements = sort.runOneStepInsertionSort(elements, els.length - 5);
-        System.out.println(elements );
+
         assertTrue(twoArraysSame(els5, elements));
 
 
-        Integer[] els6 =
-          {
+        Integer[] els6 = {
             6, 1, 3, 4, 5, 7, 8
-          };
+        };
 
         elements = sort.runOneStepInsertionSort(elements, els.length - 6);
         assertTrue(twoArraysSame(els6, elements));
 
-        Integer[] els7 =
-          {
+        Integer[] els7 = {
             1, 3, 4, 5, 6, 7, 8
-          };
+        };
 
         elements = sort.runOneStepInsertionSort(elements, els.length - 7);
         assertTrue(twoArraysSame(els7, elements));
-      }
+    }
 
-    private static final boolean twoArraysSame(Integer[] one, List<Integer> two)
-      {
-        if (one.length != two.size())
-          {
+    private static final boolean twoArraysSame(Integer[] one, List<Integer> two) {
+        if (one.length != two.size()) {
             return false;
-          }
-        for (int i = 0; i < one.length; i++)
-          {
-            if (one[i] != two.get(i))
-              {
+        }
+        for (int i = 0; i < one.length; i++) {
+            if (one[i] != two.get(i)) {
                 return false;
-              }
-          }
+            }
+        }
         return true;
-      }
-  }
+    }
+
+    /**
+     * Test of runFullMergeSort method, of class Sorting.
+     */
+    @Test
+    public void testRunFullMergeSort() {
+
+        Integer[] els = {
+            6, 5, 3, 1, 8, 7, 4
+        };
+
+        Integer[] elsCorrect = {1, 3, 4, 5, 6, 7, 8};
+        elements = Sorting.runFullMergeSort(elements);
+      
+
+        assertTrue(twoArraysSame(elsCorrect, elements));
+
+    }
+}
--- SteeringBehaviors/test/com/me/utility/UtilityTest.java
+++ SteeringBehaviors/test/com/me/utility/UtilityTest.java
@@ -15,32 +15,65 @@ import static org.junit.Assert.*;
  *
  * @author James Moore (moore.work@live.com)
  */
-public class UtilityTest
-  {
+public class UtilityTest {
 
     List<Integer> vals;
-    Utility ut; 
+    List<Integer> vals2;
+    Utility ut;
 
-    public UtilityTest()
-      {
-      }
+    public UtilityTest() {
+    }
 
     @Before
-    public void setUp()
-      {
-        Integer[] els =
-          {
-            1,2,4
-          };
+    public void setUp() {
+        Integer[] els = {
+            1, 2, 4
+        };
+        Integer[] els2 = {5, 1000};
         vals = Arrays.asList(els);
+        vals2 = Arrays.asList(els2);
         ut = new Utility();
-      }
+    }
 
     @Test
-    public void testInsertAt()
-      {
-          System.out.println(vals);
+    public void testInsertAtMiddle() {
+        
         List<Integer> foo = ut.insertAt(vals, 2, new Integer(3));
-        System.out.println(foo);
-      }
-  }
+       
+        assertEquals((Integer) 1, foo.get(0));
+        assertEquals((Integer) 2, foo.get(1));
+        assertEquals((Integer) 3, foo.get(2));
+        assertEquals((Integer) 4, foo.get(3));
+    }
+    
+    
+    @Test
+    public void testInsertTooBig() {
+       
+        try{
+            
+        List<Integer> foo = ut.insertAt(vals, vals.size() + 1, new Integer(3));
+       fail("Should throw exception");
+        }catch(IllegalArgumentException e){
+            //Good path
+        }
+    }
+
+    /**
+     * Test of concat method, of class Utility.
+     */
+    @Test
+    public void testConcat() {
+        Utility util = new Utility();
+
+        List<Integer> foo = new LinkedList<Integer>();
+
+        foo = util.concat(vals, vals2);
+        // System.out.println(foo);
+        assertEquals((Integer) 1, foo.get(0));
+        assertEquals((Integer) 2, foo.get(1));
+        assertEquals((Integer) 4, foo.get(2));
+        assertEquals((Integer) 5, foo.get(3));
+        assertEquals((Integer) 1000, foo.get(4));
+    }
+}
