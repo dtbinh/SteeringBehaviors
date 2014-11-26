4340e407fe66cf197705236556c32d8a9c82f75c
moorework <moore.work@live.com>
2014-11-25 20:57:11 -0500

    Settings screen decoupling

--- src/main/java/com/me/steeringbehaviors/Settings.java
+++ src/main/java/com/me/steeringbehaviors/Settings.java
@@ -2,86 +2,79 @@ package com.me.steeringbehaviors;
 
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.GraphicsEnvironment;
 import java.awt.Toolkit;
 
 /**
- * Settings class, get over it.  
+ * Settings class, get over it.
+ *
  * @author moore
  */
-public class Settings
-{
+public class Settings {
 
     private final static Settings instance = new Settings();
-    private final static Dimension worldSize = new Dimension(2000,1000);
+    private final static Dimension worldSize = new Dimension(2000, 1000);
     private static final int RADIUS_SCALE_CONST = 10;
     private Dimension res;
     private static int num;
 
-    private Settings()
-    {
-             Toolkit tk = Toolkit.getDefaultToolkit();
-         res = tk.getScreenSize();
+    private Settings() {
+        GraphicsEnvironment env = GraphicsEnvironment.getLocalGraphicsEnvironment();
+        if (env.isHeadlessInstance() == false) {
+            Toolkit tk = Toolkit.getDefaultToolkit();
+            res = tk.getScreenSize();
+        } else {
+            res = worldSize;
+        }
 
     }
 
-    public static Settings getInstance()
-    {
+    public static Settings getInstance() {
         return instance;
     }
 
-    public Color getBackgroundColor()
-    {
+    public Color getBackgroundColor() {
         return Color.gray;
     }
 
-    public double getDesiredFPS()
-    {
+    public double getDesiredFPS() {
         return 60;
     }
 
-    public double getElasticity()
-    {
+    public double getElasticity() {
         return 0;
     }
 
-    public double getWorldWidth()
-    {
+    public double getWorldWidth() {
         return worldSize.getWidth();
     }
 
-    public double getRadiusScale()
-    {
+    public double getRadiusScale() {
         return RADIUS_SCALE_CONST;
     }
 
-    public double getWorldHeight()
-    {
+    public double getWorldHeight() {
         return worldSize.getHeight();
     }
- 
-    public String getBallFileName()
-    {
+
+    public String getBallFileName() {
         return "Balls.ini";
     }
 
-    public void setResolution(Dimension resolution)
-    {
+    public void setResolution(Dimension resolution) {
         res = resolution;
     }
 
-    public Dimension getResolution()
-    {
+    public Dimension getResolution() {
         return res;
     }
 
-    public Dimension getWorldSize()
-    {
+    public Dimension getWorldSize() {
         return worldSize;
     }
 
-    public int getNumber()
-      {
+    public int getNumber() {
         num += 1;
         return num;
-      }
+    }
 }
